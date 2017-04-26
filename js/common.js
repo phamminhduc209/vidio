07416899f6597f11d0af3747ab80ff76e79bc008
Ducpm <minhducpham.it@gmail.com>
2017-04-26 17:08:54 +0700

    add

--- js/common.js
+++ js/common.js
@@ -1,10 +1,30 @@
 (function($){
   "use strict";
 
-  // Show Menu Hover
+	// When you hover over menu, the background of the body will be background: rgba(0,0,0,0.8);
+	$('.sidebar_menu-row-collapsible')
+	.mouseenter(function() {
+		$('body').find(".overlay").show();
+	})
+	.mouseleave(function() {
+		$('body').find(".overlay").hide();
+	});
 
-  // Show suggestion saerch
+	// Show suggestion saerch
   
+	// ===== Scroll to Top ==== 
+	$(window).scroll(function() {
+		if ($(this).scrollTop() >= 200) {
+	  		$('#return-to-top').addClass('td-scroll-up-visible');
+		} else {
+	  		$('#return-to-top').removeClass('td-scroll-up-visible');
+		}
+	});
+	$('#return-to-top').click(function() {
+		$('body,html').animate({
+	  		scrollTop : 0
+		}, 'slow');
+	});
   
 
 })(jQuery); // End of use strict
